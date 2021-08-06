496f1fe951d4fcaeea22b91d446522afc761310c
youngradix <robinsonacosta1@gmail.com>
2021-08-06 01:02:49 -0400

    Create isleapyear.c

---
+++ isleapyear.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+    int year;
+    if (year % 4 == 0) {
+        if (year % 100 == 0) {
+            if (year % 400 == 0){
+                printf("%d was a leap year", year);
+            }
+            else{
+               printf("%d was not a leap year", year);
+            }
+        }
+        else{
+            printf("%d was a leap year", year);
+        }
+    }
+    else{
+        printf("%d was not a leap year", year);
+    }
+}
