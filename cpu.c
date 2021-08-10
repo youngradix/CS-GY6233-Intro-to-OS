b2529cacab0e57bfb56c826ec5943abb874fa197
youngradix <robinsonacosta1@gmail.com>
2021-08-10 01:33:50 -0400

    Update cpu.c

--- cpu.c
+++ cpu.c
@@ -21,10 +21,10 @@ struct PCB handle_process_arrival_pp(struct PCB ready_queue[QUEUEMAX], int *queu
     if((current_process.process_id == 0) && (current_process.arrival_timestamp == 0) && (current_process.total_bursttime == 0) 
     && (current_process.execution_starttime == 0) && (current_process.execution_endtime == 0)&& (current_process.remaining_bursttime == 0) 
     && (current_process.process_priority == 0){
-
-        
-        
-    
+        new_process.execution_starttime = timestamp;
+        new_process.execution_endtime = timestamp = new_process.total_bursttime;
+        new_process.remaining_bursttime = new_process.total_bursttime;
+        return new_process;
     }
     /*If there is a currently-running process, the method compares the priority of the newly-arriving process with the currently-running
     process. If the new process has equal or lower priority (smaller integers for the priority field in the PCB indicate higher priority),
@@ -32,20 +32,28 @@ struct PCB handle_process_arrival_pp(struct PCB ready_queue[QUEUEMAX], int *queu
     process is added to the ready queue, its execution start time and execution end time are set to 0, and the remaining burst time is the 
     same as its total burst time.*/
     if (new_process.process_priority >= current_process.process.priority){
-
+        new_process.execution_starttime = 0;
+        new_process.execution_endtime = 0;
+        new_process.remaining_bursttime = new_process.total_bursttime;
+        ready_queue[*queue_cnt] = new_process;
+        *queue_cnt = *queue_cnt + 1;
+        return current_process;
     }
     /*If the new process has a higher priority, then the PCB of the currently-running process is added to the ready queue and the return 
     value is the PCB of the new process. In this case, the PCB of the new process is modified so that the execution start time is set to 
     the current timestamp, the execution end time is set to the sum of the current timestamp and the total burst time and the remaining 
     burst time is set to the total burst time. Also, the PCB of the currently-running process is added to the ready queue after marking 
     its execution end time as 0, and adjusting its remaining burst time.    */
-    else{
-
-
-
-
-    }
-   
+    else{//higher means a lower number so <=
+        new_process.execution_starttime = timestamp;
+        new_process.execution_endtime = timestamp + new_process.total_bursttime;
+        new_process.remaining_bursttime = new_process.total_bursttime;
+        current_process.remaining_bursttime = current_process.execution_endtime - timestamp;
+        current_process.execution_endtime = 0;
+        ready_queue[*queue_cnt] = current_process;
+        *queue_cnt = *queue_cnt +1;
+        return new_process;
+    } 
 }
 
 struct PCB handle_process_completion_pp(struct PCB ready_queue[QUEUEMAX], int *queue_cnt, int timestamp);{
