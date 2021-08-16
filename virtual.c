5971bf40e3840cffb015c264be7c8bc43eca8232
youngradix <robinsonacosta1@gmail.com>
2021-08-15 23:21:22 -0400

    Update virtual.c

--- virtual.c
+++ virtual.c
@@ -53,18 +53,18 @@ int process_page_access_fifo(struct PTE page_table[TABLEMAX],int *table_cnt, int
                 temp_arrival_stamp = page_table[i].arrival_timestamp;
                 temp_index = i;
             }
-        }
-        if(!flag){
-            page_table[temp_index].is_valid = 0;
-            page_table[temp_index].frame_number = -1;
-            page_table[temp_index].arrival_timestamp = 0;
-            page_table[temp_index].last_access_timestamp = 0;
-            page_table[temp_index].reference_count = 0;
-            page_table[page_number].is_valid = 1;
-            page_table[page_number].frame_number = temp_frame;
-            page_table[page_number].arrival_timestamp = current_timestamp;
-            page_table[page_number].last_access_timestamp = current_timestamp;
-            page_table[page_number].reference_count = 1;
+            if(!flag){
+                page_table[temp_index].is_valid = 0;
+                page_table[temp_index].frame_number = -1;
+                page_table[temp_index].arrival_timestamp = 0;
+                page_table[temp_index].last_access_timestamp = 0;
+                page_table[temp_index].reference_count = 0;
+                page_table[page_number].is_valid = 1;
+                page_table[page_number].frame_number = temp_frame;
+                page_table[page_number].arrival_timestamp = current_timestamp;
+                page_table[page_number].last_access_timestamp = current_timestamp;
+                page_table[page_number].reference_count = 1;
+            }
         }
     return temp_frame;
     }
